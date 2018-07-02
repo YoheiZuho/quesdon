c5ca650e1a6b05f21f87f2ef1c2f12ea231d4ea1
YoheiZuho <yuuman002@gmail.com>
2018-07-02 14:48:34 +0900

    Update footer.tsx
    サーバー固有のエラー報告・ログイン限定へのクレームが公式アカウントへ行かないよう、Quesdon公式アカウントへのリンクを廃止させていただきました。

--- src/client/components/footer.tsx
+++ src/client/components/footer.tsx
@@ -7,13 +7,6 @@ export class Footer extends React.Component {
         return <footer className="container">
             <p>Quesdon は AGPL-3.0 で提供されています。<a href={upstreamUrl}>ソースコード</a>&nbsp;
             (<a href={`${upstreamUrl}/commits/${gitVersion}`}>{gitVersion.slice(0, 7)}</a>)</p>
-            <p>
-                公式アカウント: <a href="https://mstdn.jp/@quesdon">@quesdon@mstdn.jp</a>
-                &nbsp;
-                <a href="https://twitter.com/quesdon">@Quesdon on Twitter</a>
-                &nbsp;
-                <Link to="/@quesdon@mstdn.jp">公式Quesdon (要望や不具合報告もこちらへどうぞ)</Link>
-            </p>
             <p>開発者: <a href="https://mstdn.maud.io/@rinsuki">@rinsuki@mstdn.maud.io</a></p>
             <p>
                 {usingDarkTheme
